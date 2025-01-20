dc2052854e22ba51ca1f3efbecdaa7e7f0bb4e76
VIKTORYIA SAMSONAVA <viktoriya09june@icloud.com>
2025-01-20 12:37:45 +0300

    item entity

---
+++ TaskListViper/TaskListViper/Entities/ToDoItem.swift
@@ -0,0 +1,22 @@
+//
+//  ToDoItem.swift
+//  TaskListViper
+//
+//  Created by Victoria Samsonova on 20.01.25.
+//
+
+import Foundation
+
+class ToDoItem {
+    var id: Int
+    var todo: String
+    var completed: Bool
+    var userId: Int
+    
+    init(id: Int, todo: String, completed: Bool, userId: Int) {
+        self.id = id
+        self.todo = todo
+        self.completed = completed
+        self.userId = userId
+    }
+}
